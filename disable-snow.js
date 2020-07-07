54f2b0ed4eade707c9e3818fc60296e3a8fdb221
wight-airmash <38955076+wight-airmash@users.noreply.github.com>
2020-07-08 01:51:19 +0300

    Add an extension.

---
+++ disable-snow.js
@@ -0,0 +1,11 @@
+(function () {
+  Christmas2017Theme.addSnow = function () {};
+
+  SWAM.registerExtension({
+    name: 'Disable Snow Effects',
+    id: 'wight.disableSnow',
+    description: 'Disable SWAM snow effects.',
+    version: '1.0.0',
+    author: 'wight',
+  });
+})();
